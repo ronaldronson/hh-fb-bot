175fbc044be469d2fbba756d470e7be868e5741e
ronaldronson <romenskyi.roman@gmail.com>
2016-05-23 06:12:32 +0200

    master: fix rest link

--- mapper.js
+++ mapper.js
@@ -5,7 +5,7 @@ module.exports = {
     "image_url": "https://static.hungryhouse.co.uk/images/restlogos/" + rest.id + ".gif",
     "buttons": [{
       "type": "web_url",
-      "url": "https://hungryhouse.co.uk/basel-pizza-london?postcode=" + postcode,
+      "url": "https://hungryhouse.co.uk/" + rest.name.toLowerCase().replace(/\s/g , '-') + "?postcode=" + postcode,
       "title": "See menu"
     }]
   }))
