f8832c7dce50e0ef51deb3a46c0f95484f754ae7
ronaldronson <romenskyi.roman@gmail.com>
2016-05-26 06:58:13 +0200

    master: added missing file

---
+++ db.js
@@ -0,0 +1,17 @@
+const MongoClient = require('mongodb').MongoClient
+const connStr = require('./config').mongo_db
+
+module.exports = (onready) => {
+  MongoClient.connect(connStr, (err, db) => {
+    if (err) return onready(err)
+
+    const collection = db.collection('users');
+
+    onready(null, {
+      close: () => db.close(),
+      updateOne: (key, data, done) => collection.updateOne({sender: key}, {$set: data}, done),
+      insert: (data, done) => collection.insertOne(data, done),
+      find: (key, done) => collection.findOne({sender: key}, done)
+    })
+  });
+}
