6e764e6b33682f687ca97a4a4afd236a74960ddf
ronaldronson <romenskyi.roman@gmail.com>
2016-06-02 06:25:48 +0200

    master: changed rest link using webname

--- mapper.js
+++ mapper.js
@@ -5,7 +5,7 @@ module.exports = {
     "image_url": "https://static.hungryhouse.co.uk/images/restlogos/" + rest.id + ".gif",
     "buttons": [{
       "type": "web_url",
-      "url": "https://hungryhouse.co.uk/" + rest.name.toLowerCase().replace(/\s/g , '-') + "?postcode=" + postcode,
+      "url": "https://hungryhouse.co.uk/" + rest.webname + "?postcode=" + postcode,
       "title": "See menu"
     }]
   }))
